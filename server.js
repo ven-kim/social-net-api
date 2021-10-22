fdbfb07917280968983535c089ea1673610e7324
Ven Kim <venvex@gmail.com>
2021-10-21 23:01:24 -0400

    created routes and api files

--- server.js
+++ server.js
@@ -5,3 +5,18 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use(require("./routes"));
+
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/social-network",
+  {
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
+
+mongoose.set("debug", true);
+
+app.listen(PORT, () => console.log(`Connected on Localhost: ${PORT}`));
